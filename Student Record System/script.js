750f0e4b8fb69b1f7444c4a3164f7cd32697acda
Abin VK <vettikkadanabin@gmail.com>
2026-05-15 19:55:11 +0530

    project

--- Student Record System/script.js
+++ Student Record System/script.js
@@ -1,1 +1,21 @@
 let students = [];
+
+function addStudent(){
+
+    let name = document.getElementById('name').value;
+
+    let roll = Number(document.getElementById("roll").value);
+
+    let marks = Number(document.getElementById("marks").value);
+
+    if(name === "" || roll === "" || marks === ""){
+
+        alert("Please fill all the fields");
+
+        return;
+
+    }
+
+    
+
+}
