39d34962bc6bb3ce3aa468b4a50b9162f4868392
Abin VK <vettikkadanabin@gmail.com>
2026-05-15 21:06:39 +0530

    project

--- Student Record System/script.js
+++ Student Record System/script.js
@@ -1,5 +1,7 @@
 let students = [];
 
+
+// Add Student
 function addStudent(){
 
     let name = document.getElementById('name').value;
@@ -8,28 +10,36 @@ function addStudent(){
 
     let marks = Number(document.getElementById("marks").value);
 
+
     if(name === "" || roll === "" || marks === ""){
 
         alert("Please fill all the fields");
 
         return;
-
     }
 
     let grade;
 
     if(marks >= 90){
+
         grade = "A+";
 
-    } else if(marks >= 75){
+    } 
+    else if(marks >= 75){
+
         grade = "A";
 
-    } else if(marks >= 50){
+    } 
+    else if(marks >= 50){
+
         grade = "B";
 
-    } else {
+    } 
+    else {
+
         grade = "Fail";
     }
+
 
     let student = {
 
@@ -38,61 +48,74 @@ function addStudent(){
         roll: roll,
 
         marks: marks,
-        
+
         grade: grade
     };
+
 
     students.push(student);
 
     displayStudents();
 
     clearInputs();
+}
 
-    function displayStudents(){
 
-        let studentList = document.getElementById("studentList");
 
-        studentList.innerHTML = "";
-        
-        for(let i = 0; i < students.length; i++){
+// Display Students
+function displayStudents(){
 
-            studentList.innerHTML += `
+    let studentList = document.getElementById("studentList");
 
-            <div class="student-card">
+    studentList.innerHTML = "";
+    
 
-                <p><strong>Name:</strong> ${students[i].name}</p>
-                <p><strong>Roll:</strong> ${students[i].roll}</p>
-                <p><strong>Marks:</strong> ${students[i].marks}</p>
-                <p><strong>Grade:</strong> ${students[i].grade}</p>
+    for(let i = 0; i < students.length; i++){
 
-                <button class="delete-btn" onclick="deleteStudent(${students[i].roll})">
-                   Delete
-               </button>
+        studentList.innerHTML += `
 
-            </div>
-            `;
+        <div class="student-card">
+
+            <p><strong>Name:</strong> ${students[i].name}</p>
+
+            <p><strong>Roll:</strong> ${students[i].roll}</p>
+
+            <p><strong>Marks:</strong> ${students[i].marks}</p>
+
+            <p><strong>Grade:</strong> ${students[i].grade}</p>
+
+            <button class="delete-btn" onclick="deleteStudent(${students[i].roll})">
+
+                Delete
+
+            </button>
+
+        </div>
+        `;
+    }
+}
+
+
+
+// Delete Student
+function deleteStudent(roll){
+
+    for(let i = 0; i < students.length; i++){
+
+        if(students[i].roll === roll){
+
+            students.splice(i, 1);
+
+            displayStudents();
+
+            return;
         }
     }
-
-
-    function deleteStudent(roll){
-
-        for(let i = 0; i < students.length; i++){
-
-            if(students[i].roll === roll){
-
-                students.splice(i, 1);
-
-                displayStudents();
-
-                return;
-            }
-
-    }
-}
 }
 
-         
+
+
+// Clear Inputs
 function clearInputs(){
 
     document.getElementById("name").value = "";
