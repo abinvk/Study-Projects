0e834750ac64d9254986d73087965ddedd3685f0
Abin VK <vettikkadanabin@gmail.com>
2026-05-22 22:27:48 +0530

    study

--- Session Manager/script.js
+++ Session Manager/script.js
@@ -5,5 +5,27 @@ function login(){
 
     sessionStorage.setItem("user", username);
     sessionStorage.setItem("time", new Date());
-    status.textContent = "Welcome" + username;
+    status.textContent = "Welcome " + username;
+
+
+    // Auto logout after 60 seconds
+
+    setTimeout(() => {
+        logout();
+    }, 60000); // 60 seconds
+
+}
+
+function logout(){
+    sessionStorage.clear();
+    status.textContent = "You have been logged out.";
+}
+
+window.onload=()=>{
+
+    let user = sessionStorage.getItem("user");
+
+    if(user){
+        status.textContent = "Welcome back " + user;    
+    }
 }
