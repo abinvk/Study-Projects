d9e390f35d3e9ed27d649f5e1238af9bcb1334bf
Abin VK <vettikkadanabin@gmail.com>
2026-05-24 18:51:20 +0530

    study

--- Order System/script.js
+++ Order System/script.js
@@ -6,41 +6,29 @@ function addToCart(name,price){
         name:name,
         price:price
     })
-
     total+=price
-
     showCart()
 }
-
 function showCart(){
     let cartItems=document.getElementById("cardItems")
     cartItems.innerHTML=""
-
     cart.forEach((item)=>{
-
         cartItems.innerHTML+=`
         <div class="cart-item">
         <span>${item.name}</span>
         <span>${item.Price}</span>
         </div>
         `
-
     })
-
     document.getElementById("totalPrice").innerText=total
 }
-
     function placeOrder(){
         if(cart.length===0){
             alert("Cart Is Empty")
-
             return
         }
-
         alert("Order Placed Successfully")
-
         cart=[]
         total=0
-
         showCart()
     }
