2a5004eb8f610a16aa75d307e12d3f684b40ff1b
Abin VK <vettikkadanabin@gmail.com>
2026-05-24 18:48:06 +0530

    study

---
+++ Order System/script.js
@@ -0,0 +1,46 @@
+let cart=[]
+let total=0
+
+function addToCart(name,price){
+    cart.push({
+        name:name,
+        price:price
+    })
+
+    total+=price
+
+    showCart()
+}
+
+function showCart(){
+    let cartItems=document.getElementById("cardItems")
+    cartItems.innerHTML=""
+
+    cart.forEach((item)=>{
+
+        cartItems.innerHTML+=`
+        <div class="cart-item">
+        <span>${item.name}</span>
+        <span>${item.Price}</span>
+        </div>
+        `
+
+    })
+
+    document.getElementById("totalPrice").innerText=total
+}
+
+    function placeOrder(){
+        if(cart.length===0){
+            alert("Cart Is Empty")
+
+            return
+        }
+
+        alert("Order Placed Successfully")
+
+        cart=[]
+        total=0
+
+        showCart()
+    }
