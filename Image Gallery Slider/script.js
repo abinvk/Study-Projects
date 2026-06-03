96fd62c768904442cc89cd599c0d6c656ed878ed
Abin VK <vettikkadanabin@gmail.com>
2026-06-03 19:26:05 +0530

    study

---
+++ Image Gallery Slider/script.js
@@ -0,0 +1,22 @@
+const preview = document.getElementById("preview");
+const thumbnails = document.querySelectorAll(".thumb");
+const nextBtn = document.getElementById("nextBtn");
+const prevBtn = document.getElementById("preBtn");
+
+let currentIndex = 0;
+
+const images = [
+    "https://picsum.photos/id/1015/600/400",
+    "https://picsum.photos/id/1016/600/400",
+    "https://picsum.photos/id/1018/600/400",
+    "https://picsum.photos/id/1020/600/400"
+];
+
+function showImage(index) {
+    preview.src = images[index];
+
+    thumbnails.forEach((thumb)=>{
+        thumb.classList.remove("active");
+    });
+    thumbnails[index].classList.add("active");
+}
