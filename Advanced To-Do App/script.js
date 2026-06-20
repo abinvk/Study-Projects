8c0da5ad7e14ef4b73ed3c2fc6d31cfce8b322aa
Abin VK <vettikkadanabin@gmail.com>
2026-06-20 19:11:46 +0530

    study

--- Advanced To-Do App/script.js
+++ Advanced To-Do App/script.js
@@ -4,3 +4,17 @@ const taskContainer = document.getElementById("taskContainer");
 const searchInput = document.getElementById("searchInput");
 const liveText = document.getElementById("liveText");
 const themBtn = document.getElementById("themeBtn");
+
+// // Input Value Display
+
+taskInput.addEventListener("input",()=>{
+    liveText.textContent = taskInput.value;
+});
+
+// Disable / Enable Button
+
+taskInput.addEventListener("input",()=>{
+    addTaskBtn.disabled = taskInput.value.trim()==="";
+});
+
+addTaskBtn.disabled = true;
