3b90fb399071de07510f0b1e7a676ad4d3c5b155
Abin VK <vettikkadanabin@gmail.com>
2026-06-20 19:01:17 +0530

    study

---
+++ Advanced To-Do App/script.js
@@ -0,0 +1,6 @@
+const taskInput = document.getElementById("taskInput");
+const addTaskBtn = document.getElementById("addTaskBtn");
+const taskContainer = document.getElementById("taskContainer");
+const searchInput = document.getElementById("searchInput");
+const liveText = document.getElementById("liveText");
+const themBtn = document.getElementById("themeBtn");
