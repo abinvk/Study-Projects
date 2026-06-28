e435e3a6b4845fd4ae79ffd5cfa254d457f26301
Abin VK <vettikkadanabin@gmail.com>
2026-06-28 19:38:13 +0530

    study

--- Counter App/script.js
+++ Counter App/script.js
@@ -5,3 +5,29 @@ const reset = document.getElementById("reset");
 const statusText = document.getElementById("status");
 
 let number = 0;
+
+function updateUI(){
+    count.textContent = number;
+    if(number > 0){
+        statusText.textContent = "Positive 😊";
+        count.style.color = "#00ff99";
+
+    }else{
+
+        if(number < 0){
+            statusText.textContent = "Negative 😔";
+            count.style.color = "#ff1744"
+
+        }else{
+            statusText.textContent = "Zero 😎";
+            count.style.color = "#ffffff";
+        }
+    }
+
+    increase.addEventListener("click",()=>{
+        number++;
+        updateUI();
+    });
+
+
+}
