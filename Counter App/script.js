501a902d18e04833a6be8ca5c23490aefce19505
Abin VK <vettikkadanabin@gmail.com>
2026-06-28 19:25:31 +0530

    study

---
+++ Counter App/script.js
@@ -0,0 +1,7 @@
+const count = document.getElementById("count");
+const increase = document.getElementById("increase");
+const decrease = document.getElementById("decrease");
+const reset = document.getElementById("reset");
+const statusText = document.getElementById("status");
+
+let number = 0;
