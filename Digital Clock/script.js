2ec683ebba90ba0186101174bfe2b36b91268e8f
Abin VK <vettikkadanabin@gmail.com>
2026-06-29 23:51:14 +0530

    study

--- Digital Clock/script.js
+++ Digital Clock/script.js
@@ -6,5 +6,19 @@ const date = document.getElementById("date");
 function updateClock(){
     const now = new Date();
 
-    
+    let hr = now.getHours();
+    let min = now.getMinutes();
+    let sec = now.getSeconds();
+
+    hr = String(hr).padStart(2,"0");
+    min = String(min).padStart(2,"0");
+    sec = String(sec).padStart(2,"0");
+
+    hours.textContent = hr;
+    minutes.textContent = min;
+    seconds.textContent = sec;
+
+    date.textContent = now.toDateString()
 }
+updateClock();
+setInterval(updateClock,1000);
