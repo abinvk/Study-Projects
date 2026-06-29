b624aab0fe0f84f592c9342bc79ac84f6265d95b
Abin VK <vettikkadanabin@gmail.com>
2026-06-29 23:43:57 +0530

    study

--- Digital Clock/script.js
+++ Digital Clock/script.js
@@ -2,3 +2,9 @@ const hours = document.getAnimations("hours");
 const minutes = document.getElementById("minutes");
 const seconds = document.getElementById("seconds");
 const date = document.getElementById("date");
+
+function updateClock(){
+    const now = new Date();
+
+    
+}
